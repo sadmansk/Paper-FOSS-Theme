1cf7ac70ce508f34af67be9ab3e9498246b3a184
dkanada <dkanada@users.noreply.github.com>
2018-01-26 06:43:45 -0700

    fix svg script

--- svgs/svg.sh
+++ svgs/svg.sh
@@ -3,20 +3,24 @@
 # Thanks to t184256
 # Requires Inkscape and Imagemagic
 
-for SVG in new/*.svg
+PNGDIR="../app/src/main/res"
+NEWDIR="new"
+MVDIR="done"
+
+for SVG in ${NEWDIR}/*.svg
 do
     N=$(basename $SVG .svg)
     inkscape -f $SVG -w 48 -h 48 -e \
-        app/src/main/res/drawable-mdpi/$N.png
+        ${PNGDIR}/drawable-mdpi/$N.png
     inkscape -f $SVG -w 72 -h 72 -e \
-        app/src/main/res/drawable-hdpi/$N.png
+        ${PNGDIR}/drawable-hdpi/$N.png
     inkscape -f $SVG -w 96 -h 96 -e \
-        app/src/main/res/drawable-xhdpi/$N.png
+        ${PNGDIR}/drawable-xhdpi/$N.png
     inkscape -f $SVG -w 144 -h 144 -e \
-        app/src/main/res/drawable-xxhdpi/$N.png
+        ${PNGDIR}/drawable-xxhdpi/$N.png
     inkscape -f $SVG -w 192 -h 192 -e \
-        app/src/main/res/drawable-xxxhdpi/$N.png
+        ${PNGDIR}/drawable-xxxhdpi/$N.png
     inkscape -f $SVG -w 384 -h 384 -e \
-        app/src/main/res/drawable-nodpi/nodpi_$N.png
-    mv $SVG "done/"
+        ${PNGDIR}/drawable-nodpi/nodpi_$N.png
+    mv ${SVG} ${MVDIR}
 done
